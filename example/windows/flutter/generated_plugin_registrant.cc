ffa4bdc23b2bf059f15ad8b9044b2349c2cf6ef3
kamil.karpacz <kamilkarpacz96@gmail.com>
2023-05-17 23:07:36 +0200

    update plugin

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <win32_registry_observer/win32_registry_observer_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  Win32RegistryObserverPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("Win32RegistryObserverPluginCApi"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  win32_registry_observer
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
